186c5ea6913ae5ef165dca70f227dd358b7b4095
uniqueuseraccount <77390578+uniqueuseraccount@users.noreply.github.com>
2025-08-19 13:43:46 -0500

    Update and rename db_credentials.py to db_credentials_example.py

---
+++ backend/db_credentials_example.py
@@ -0,0 +1,11 @@
+# rename this file to db_credentials.py
+#
+# Enter your MySQL database credentials here.
+# This file is imported by the application and should be kept secure.
+
+DB_CONFIG = {
+    'host': 'INSERT HOST',
+    'user': 'INSERT USER',
+    'password': 'INSERT PASSWORD',
+    'database': 'INSERT DATABASE NAME'
+}
